4da49abfc0745967a13f7a9ddfc103cb84ab5b89
albert748 <albert748@gmail.com>
2012-03-16 10:45:14 +0800

    i18n for popup.html
    1. move all translate functions to wizChromExtension.js
    2. add chinese translation to messages.json
    3. adjust popup.html

---
+++ script/wizChromeExtension.js
@@ -0,0 +1,88 @@
+function localizeBlock(block) {
+    if (block.attr("message"))
+        localizeElement(block);
+    var siblings = block.find("[message], [placeholder], [title]");
+    for ( var i = 0; i < siblings.length; i++) {
+        var sibling = $(siblings.get(i));
+        localizeElement(sibling);
+    }
+}
+
+function localizeElement(element, force) {
+    if (!force && element.attr("localized")
+        && element.attr("localized") == "true") {
+            return;
+    }
+
+    var field = extractLocalizationField(element);
+    var placeholderField = extractLocalizationPlaceholderField(element);
+    var titleField = extractLocalizationTitleField(element);
+    var fieldData = extractLocalizationDataField(element);
+    var localized = false;
+
+    if (field) {
+        if (fieldData) {
+            var msg = chrome.i18n.getMessage(field, fieldData);
+        } else {
+            var msg = chrome.i18n.getMessage(field);
+        }
+        if (element.attr("tagName") == "INPUT") {
+            element.attr("value", msg);
+        } else {
+            element.html(msg);
+        }
+        localized = true;
+    }
+
+    if (placeholderField) {
+        var msg = chrome.i18n.getMessage(placeholderField);
+        element.attr("placeholder", msg);
+        localized = true;
+    }
+
+    if (titleField) {
+        var msg = chrome.i18n.getMessage(titleField);
+        element.attr("title", msg);
+        localized = true;
+    }
+
+    if (localized) {
+        element.attr("localized", "true");
+    }
+}
+
+function extractLocalizationField(element) {
+    if (typeof element.attr == 'function' && element.attr("message"))
+        return element.attr("message");
+    else
+        return null;
+}
+
+function extractLocalizationPlaceholderField(element) {
+    if (typeof element.attr == 'function' && element.attr("placeholder"))
+        return element.attr("placeholder");
+    else
+        return null;
+}
+
+function extractLocalizationTitleField(element) {
+    if (typeof element.attr == 'function' && element.attr("title"))
+        return element.attr("title");
+    else
+        return null;
+}
+
+function extractLocalizationDataField(element) {
+    if (typeof element.attr == 'function' && element.attr("messagedata")) {
+        var v = element.attr("messagedata");
+        try {
+            v = eval(v);
+        } catch (e) {}
+        if (!(v instanceof Array))
+            v = [ v ];
+            return v;
+        } else {
+            return null;
+    }
+}
+
